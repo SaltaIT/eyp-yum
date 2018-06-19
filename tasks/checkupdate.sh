812bb4c6ad8cd410ca3f27b28496fbf24f590564
Jordi Prats <jordi.prats@gmail.com>
2018-06-19 16:27:30 +0200

    rename

--- tasks/checkupdate.sh
+++ tasks/checkupdate.sh
@@ -2,6 +2,6 @@
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 
-echo '{ "_output":'
+echo '{ "updates":'
 repoquery -q -a --qf="%{name}-%{version}-%{release}.%{arch}" --pkgnarrow=updates | awk ' BEGIN { ORS = ""; print "["; } { print "\/\@"$0"\/\@"; } END { print "]"; }' 2>/dev/null | sed "s^\"^\\\\\"^g;s^\/\@\/\@^\", \"^g;s^\/\@^\"^g"
 echo '}'
