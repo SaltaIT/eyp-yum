c3ab73dcda13834e8af516447673ff59b80c3649
Jordi Prats <jordi.prats@gmail.com>
2018-06-19 16:37:39 +0000

    updates

--- tasks/update.py
+++ tasks/update.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 
 import sys
+import json
+from pprint import pprint
+import subprocess
 
-for line in sys.stdin:
-    print line
+params = json.load(sys.stdin)
+
+packages = params['package'].split(',')
+
+result={}
+
+for package in packages:
+  result[package]={}
+  result[package]['initial']=subprocess.check_output("rpm -qa "+package, shell=True)
+  result[package]['requested']=package
+  result[package]['yum_output']=subprocess.check_output("yum update "+package+" -y", shell=True)
+  result[package]['result']=subprocess.check_output("rpm -qa "+package, shell=True)
+
+print(json.dumps(result))
