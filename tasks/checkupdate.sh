e3f5f206f9fc696a1dc23d35ac6ee58038f6ba84
Jordi Prats <jordi.prats@gmail.com>
2018-06-19 16:22:30 +0200

    _output

--- tasks/checkupdate.sh
+++ tasks/checkupdate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+
+echo '{ "_output":'
 repoquery -q -a --qf="%{name}-%{version}-%{release}.%{arch}" --pkgnarrow=updates | awk ' BEGIN { ORS = ""; print "["; } { print "\/\@"$0"\/\@"; } END { print "]"; }' 2>/dev/null | sed "s^\"^\\\\\"^g;s^\/\@\/\@^\", \"^g;s^\/\@^\"^g"
+echo '}'
