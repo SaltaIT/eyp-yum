50ce779dde045efc7899ead3c0805fe96a566e66
Jordi Prats <jordi.prats@gmail.com>
2018-06-19 16:17:37 +0200

    json output

--- tasks/checkupdate.sh
+++ tasks/checkupdate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-
-repoquery -q -a --qf="%{name}-%{version}-%{release}.%{arch}" --pkgnarrow=updates
+repoquery -q -a --qf="%{name}-%{version}-%{release}.%{arch}" --pkgnarrow=updates | awk ' BEGIN { ORS = ""; print "["; } { print "\/\@"$0"\/\@"; } END { print "]"; }' 2>/dev/null | sed "s^\"^\\\\\"^g;s^\/\@\/\@^\", \"^g;s^\/\@^\"^g"
